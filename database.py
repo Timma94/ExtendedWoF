804e5f514af7fd9ebc9004fb773ca7e9e1f8bf56
Timma94 <timvancleef@hotmail.com>
2026-09-02 14:26:09 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,12 +1,30 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
+
+# ============================================================
+# DATABASE DIRECTORY
+# ============================================================
+
+os.makedirs("data", exist_ok=True)
+
+
+# ============================================================
+# DATABASE
+# ============================================================
+
 DATABASE_URL = "sqlite:///./data/wheel_of_fortune.db"
+
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    connect_args={
+        "check_same_thread": False
+    }
 )
+
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -14,13 +32,20 @@ SessionLocal = sessionmaker(
     bind=engine
 )
 
+
 Base = declarative_base()
 
 
+# ============================================================
+# DATABASE SESSION
+# ============================================================
+
 def get_db():
+
     db = SessionLocal()
 
     try:
         yield db
+
     finally:
         db.close()
